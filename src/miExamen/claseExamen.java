6e8655703ba4481c65bdd94e05680f54705e544c
alu09v <alu09v@desarrolloweb.local>
2024-02-23 19:27:34 +0100

    Añadidos comentarios de documentación en claseExamen.

--- src/miExamen/claseExamen.java
+++ src/miExamen/claseExamen.java
@@ -1,9 +1,22 @@
 
 package miExamen;
-
+/**
+ * @title Entornos de desarrollo UT3
+ * @author Antonio
+ * @version 1.0.0
+ * Fecha crecion clase:  23/02/2024
+ * 
+ * 
+ * */
 
 public class claseExamen {
-    
+    /**
+     * Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de
+     * Desarrollo
+     * 
+     * Curso de Git y github de píldoras informáticas.
+     * https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU
+     */
     public static void main(String args[]){
         System.out.println("Estoy haciendo el examen de la UT3");
     }
